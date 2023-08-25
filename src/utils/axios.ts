20fad0b8590e489809feb8e9c7ce1a05a956963a
Sujan <sujan@aerion.com.au>
2023-08-25 19:05:27 +0545

    FIX: Set the .env file for local and prod

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const TASK_BASE_URL =
-  process.env.NODE_ENV !== "development"
+  process.env.NODE_ENV === "development"
     ? process.env.NEXT_PUBLIC_LOCAL_BASE_URL
     : process.env.NEXT_PUBLIC_PROD_BASE_URL;
 
